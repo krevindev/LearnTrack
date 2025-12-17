8ddc90cd0c4d5c58eeaea73628f65d16dd72719b
krevindev <alimpuangonkylerevin@gmail.com>
2025-12-17 10:10:48 -0800

    adjusted the maximum width

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -10,7 +10,7 @@ export default function AuthForm({ title, onSubmit, children }: AuthFormProps) {
   return (
     <form
       onSubmit={onSubmit}
-      className="w-full px-1 max-w-[400px] flex flex-col justify-center"
+      className="w-full max-w-[500px] flex flex-col justify-center border py-40 px-10 rounded-xl"
     >
       <h1 className="text-blue-600 font-bold text-3xl mb-5">{title}</h1>
       {children}
