befcd3a7723acaeba20699b780435c4038481ed9
krevindev <alimpuangonkylerevin@gmail.com>
2025-12-18 14:45:25 -0800

    Changed the auth page's bg image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,4 @@
-import signInBg from "../assets/images/bg1.jpg";
+import signInBg from "../assets/images/bg7.jpg";
 import learnTrackLogo from "../assets/logos/learntrack-logo.png";
 
 export const images = {
