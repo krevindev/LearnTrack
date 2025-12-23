e2410f1720880eecb787e399ec78157d22f70ea0
krevindev <alimpuangonkylerevin@gmail.com>
2025-12-22 22:15:34 -0800

    added essential comments

--- src/forms/SignInForm.tsx
+++ src/forms/SignInForm.tsx
@@ -5,19 +5,23 @@ import FormInput from "../components/FormInput";
 import { required } from "../utils/validation";
 import { Link } from "react-router-dom";
 
+// --- Types ---
 type Errors = {
   email?: string;
   password?: string;
 };
 
+// --- Main Component: Sign In ---
 export default function SignInForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<Errors>({});
 
+  // Handle Submit
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
+    // Initialize sign-in errors
     const newErrors: Errors = {
       email: required(email, "Email"),
       password: required(password, "Password"),
@@ -37,6 +41,7 @@ export default function SignInForm() {
   return (
     <AuthForm title="Login" onSubmit={handleSubmit}>
       <div className="flex flex-col justify-center items-stretch">
+        {/* Input: Email */}
         <FormInput
           label="Email"
           type="email"
@@ -44,7 +49,7 @@ export default function SignInForm() {
           error={errors.email}
           onChange={setEmail}
         />
-
+        {/* Input: Password */}
         <FormInput
           label="Password"
           type="password"
@@ -52,7 +57,7 @@ export default function SignInForm() {
           error={errors.password}
           onChange={setPassword}
         />
-
+        {/* Button: Submit */}
         <Button
           type="submit"
           variant="contained"
@@ -61,11 +66,11 @@ export default function SignInForm() {
           LOGIN
         </Button>
 
+        {/* Link to: Sign-up */}
         <Link
           to="/auth/sign-up"
           className="text-blue-500 mt-10 hover:underline active:underline active:translate-y-1"
         >
-          {" "}
           Create Account
         </Link>
       </div>
