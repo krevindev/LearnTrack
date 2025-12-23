7885ad4fbba8cdaf99db5235ca1eefc6797fd941
krevindev <alimpuangonkylerevin@gmail.com>
2025-12-23 10:17:20 -0800

    chore: added animate-spin speed propery extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "spin-fast": "spin 0.5s linear infinite",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
