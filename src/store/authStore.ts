685dc520ff78b4524ef0a98e30ad2ba19312654d
krevindev <alimpuangonkylerevin@gmail.com>
2025-12-27 03:41:12 +0800

    feat: initialized the zustand and authStore

---
+++ src/store/authStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { Auth, User } from "firebase/auth";
+import { loginWithEmail, logoutUser } from "../services/authServices";
+
+type AuthState = {
+  user: User | null;
+  loading: boolean;
+  error: string | null;
+
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  loading: false,
+  error: null,
+
+  login: async (email, password) => {
+    set({ loading: true, error: null });
+
+    try{
+        const user = await loginWithEmail(email, password);
+        set({user, loading: false})
+    }catch(err: any){
+        set({error: err.message ?? 'Login Failed', loadingL false})
+    }
+},
+logout: async () => {
+        await logoutUser();
+        set({user: null})
+    }
+}));
