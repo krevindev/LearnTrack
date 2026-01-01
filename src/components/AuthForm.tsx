3aee7a55834fed4cbdc49fbd8b0aff1546f4e0fe
krevindev <alimpuangonkylerevin@gmail.com>
2026-01-01 16:31:38 +0800

    fix: removed the form border

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -10,7 +10,7 @@ export default function AuthForm({ title, onSubmit, children }: AuthFormProps) {
   return (
     <form
       onSubmit={onSubmit}
-      className="w-full h-full max-w-125 py-10 px-20 rounded-xl flex flex-col justify-center items-stretch border border-black"
+      className="w-full h-full max-w-125 py-10 px-20 rounded-xl flex flex-col justify-center items-stretch"
     >
       <h1 className="text-blue-600 font-bold text-3xl mb-5">{title}</h1>
       {children}
